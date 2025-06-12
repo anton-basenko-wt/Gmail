02f8d943e45b9ba5678a95b2ee413eeff80ad1ec
Anton Basenko <anton.basenko@walltech.me>
2025-06-12 14:17:30 +0300

    Add test shoud open compose region

--- pages/mainPage.js
+++ pages/mainPage.js
@@ -3,12 +3,37 @@ import { Locator, Page } from "@playwright/test";
 export class MainPage {
     /** @type {import('@playwright/test').Page} */
     page;
+    /** @type {import('@playwright/test').Locator} */
+    composeButton;
+    /** @type {import('@playwright/test').Locator} */
+    composeRegion;
+    /** @type {import('@playwright/test').Locator} */
+    recipientsInput;
+    /** @type {import('@playwright/test').Locator} */
+    subjectInput;
+    /** @type {import('@playwright/test').Locator} */
+    /** @type {import('@playwright/test').Locator} */
+    /** @type {import('@playwright/test').Locator} */
+    /** @type {import('@playwright/test').Locator} */
+    /** @type {import('@playwright/test').Locator} */
+    /** @type {import('@playwright/test').Locator} */
+    /** @type {import('@playwright/test').Locator} */
+    /** @type {import('@playwright/test').Locator} */
+    /** @type {import('@playwright/test').Locator} */
 
     constructor(page) {
         this.page = page;
+        this.composeButton = page.getByRole('button', { name: 'Compose' });
+        this.composeRegion = page.getByRole('region', { name: 'New Message' });
+        this.recipientsInput = this.composeRegion.getByRole('combobox', { name: 'To recipients' });
+        this.subjectInput = this.composeRegion.getByPlaceholder('Subject');
     }
 
     async goto() {
         await this.page.goto('/');
+    }
+
+    async sendEmail(to, message, subject = "") {
+        await this.composeButton.click();
     }
 }
--- tests/gmail.spec.js
+++ tests/gmail.spec.js
@@ -1,14 +1,22 @@
 import { test, expect } from './fixtures/persistentContext';
 import { MainPage } from '../pages/mainPage';
 
+/** @type {import('../pages/mainPage').MainPage}*/
+let mainPage;
 
 test.describe('Gmail with context', () => {
     test.beforeEach(async ({ page }) => {
-        const mainPage = new MainPage(page);
+        mainPage = new MainPage(page);
         await mainPage.goto();
     });
 
     test('should open Gmail and verify title', async ({ page }) => {
         await expect(page).toHaveTitle(/Gmail|Mail|Inbox/i);
+    });
+
+    test('should open compose region', async ({ page }) => {
+        await expect(mainPage.composeRegion).toHaveCount(0);
+        await mainPage.composeButton.click();
+        await expect(mainPage.composeRegion).toBeVisible();
     });
 });
