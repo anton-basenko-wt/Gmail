930683d7745acee7b711ae55992f4f05c28c5aff
Anton Basenko <anton.basenko@walltech.me>
2025-06-12 17:24:55 +0300

    Add tests for sending email

--- pages/mainPage.js
+++ pages/mainPage.js
@@ -1,4 +1,4 @@
-import { Locator, Page } from "@playwright/test";
+import {Locator, Page} from "@playwright/test";
 
 export class MainPage {
     /** @type {import('@playwright/test').Page} */
@@ -12,28 +12,38 @@ export class MainPage {
     /** @type {import('@playwright/test').Locator} */
     subjectInput;
     /** @type {import('@playwright/test').Locator} */
+    messageInput;
     /** @type {import('@playwright/test').Locator} */
+    sendButton;
     /** @type {import('@playwright/test').Locator} */
-    /** @type {import('@playwright/test').Locator} */
-    /** @type {import('@playwright/test').Locator} */
-    /** @type {import('@playwright/test').Locator} */
-    /** @type {import('@playwright/test').Locator} */
-    /** @type {import('@playwright/test').Locator} */
-    /** @type {import('@playwright/test').Locator} */
+    alertDialog;
 
     constructor(page) {
         this.page = page;
-        this.composeButton = page.getByRole('button', { name: 'Compose' });
-        this.composeRegion = page.getByRole('region', { name: 'New Message' });
-        this.recipientsInput = this.composeRegion.getByRole('combobox', { name: 'To recipients' });
+        this.composeButton = this.page.getByRole('button', {name: 'Compose'});
+        this.composeRegion = this.page.getByRole('region', {name: 'New Message'});
+        this.recipientsInput = this.composeRegion.getByRole('combobox', {name: 'To recipients'});
         this.subjectInput = this.composeRegion.getByPlaceholder('Subject');
+        this.messageInput = this.composeRegion.getByRole('textbox', {name: 'Message Body'});
+        this.sendButton = this.page.getByRole('button', {name: /Send.*Enter/i});
+        this.alertDialog = this.page.getByRole('alertdialog');
     }
 
     async goto() {
         await this.page.goto('/');
     }
 
-    async sendEmail(to, message, subject = "") {
+    async sendEmail(to, subject = "", message) {
         await this.composeButton.click();
+        await this.recipientsInput.fill(to);
+        if (subject !== "") {
+            await this.subjectInput.fill(subject);
+        }
+        await this.messageInput.fill(message);
+        await this.sendButton.click();
+    }
+
+    async getMessageBySubject(subject) {
+        return this.page.locator('tr', { hasText: subject }).last();
     }
 }
--- tests/gmail.spec.js
+++ tests/gmail.spec.js
@@ -3,6 +3,7 @@ import { MainPage } from '../pages/mainPage';
 
 /** @type {import('../pages/mainPage').MainPage}*/
 let mainPage;
+let email = 'hello.there.pw@gmail.com';
 
 test.describe('Gmail with context', () => {
     test.beforeEach(async ({ page }) => {
@@ -15,7 +16,7 @@ test.describe('Gmail with context', () => {
     });
 
     test('should open compose region', async ({ page }) => {
-        await expect(mainPage.composeRegion).toHaveCount(0);
+        await expect(mainPage.composeRegion).toBeHidden();
         await mainPage.composeButton.click();
         await expect(mainPage.composeRegion).toBeVisible();
     });
@@ -23,6 +24,31 @@ test.describe('Gmail with context', () => {
     test('should close compose region', async ({ page }) => {
         await mainPage.composeButton.click();
         await page.getByRole('img', { name: 'Save & close' }).click();
-        await expect(mainPage.composeRegion).toHaveCount(0);
+        await expect(mainPage.composeRegion).toBeHidden();
+    });
+
+    test('should display error on sending email without recipient(s)', async ({ page }) => {
+        await expect(mainPage.alertDialog).toBeHidden();
+        await mainPage.sendEmail('', '', '');
+        await expect(mainPage.alertDialog).toBeVisible();
+    });
+
+    test('should display warning on sending email without subject and text', async ({ page }) => {
+        page.once('dialog', async dialog => {
+            expect(dialog.message()).toMatch(/Send.*without.*subject.*text/i);
+            await dialog.dismiss(); // или dialog.accept();
+        });
+        await mainPage.sendEmail(email, '', '');
+    });
+
+    test('should send email to self', async ({ page }) => {
+        // 7-digit long hexadecimal hash
+        const randomSubject = Math.floor(Math.random() * 0x10000000).toString(16).padStart(7, '0');
+        const text = 'Hello there!';
+        await mainPage.sendEmail(email, randomSubject, text);
+        const message = await mainPage.getMessageBySubject(randomSubject);
+        await expect(message).toBeVisible();
+        // message should contain the start of the text
+        await expect(message).toContainText(text.length > 10 ? text.substring(10) : text);
     });
 });
